95540c31283125b0cb3804ede733286b55fa4284
Germo Linder <glinde@ttu.ee>
2022-04-13 15:45:17 +0300

    added speaker

--- src/components/ic_components/Speakers/Speakers.js
+++ src/components/ic_components/Speakers/Speakers.js
@@ -2,8 +2,15 @@ export const speakers = [
   {
     image: 'https://i.imgur.com/4htPKHK.jpg',
     name: 'Laura Leon',
-    company: 'Wise',
+    company: 'Product Designer @ Wise',
     title: 'Building for people without borders - How to approach design with inclusion',
     text: 'Laura has worked with startups as a full-stack designer for a little over 9 years, last three of those, at Wise. She’s passionate about making design processes more inclusive - for customers and for anyone who’d like to solve problems people have. She’ll talk about the role of inclusion in building products for everybody, and how that can be applied in projects at any scale.'
+  },
+  {
+    image: 'https://i.imgur.com/w00Zq3U.jpg',
+    name: 'Raimo Seero',
+    company: 'CTO @ Uptime',
+    title: 'How to develop for speed and snappiness?',
+    text: 'Raimo has been involved with computers, IT-systems, and software development in one way or another for over 20 years. Today, he serves as the CTO of Uptime, one of the largest software development companies in Estonia, and is responsible for ensuring the use of best development practices, advising and coaching development teams, as well as overseeing and developing Uptime’s tech stack. At Konfluence, Raimo will talk about front-end development, focusing on speed, responsiveness, and overall snappiness. How to achieve the best results, which pitfalls to look out for, and how to utilize best practices in every step of the way? All of this and much-much more will be revealed.'
   },
 ];
